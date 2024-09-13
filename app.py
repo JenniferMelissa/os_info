1b41bd01624380db41b45a8f30dabd9649947518
jennifermel <jennifermelelissa8503@gmail.com>
2024-09-13 17:36:43 -0300

    incluindo biblioteca para trazer o nome do sistema operacional e sua versao

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
+import platform 
 
 #pega qual o nome de usuario que voce esta usando e colocar na variavel criada
 usuario = os.environ.get('USERNAME')
 #nt = windows 
 
-print(f'Sistema Operacional: {os.name}.')
+print(f'Sistema Operacional: {platform.system()} {platform.release()}.')
 print(f'Nome do usuário: {usuario}.')
